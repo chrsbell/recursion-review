72f7a4f6dbe15aaa66d08eefcd35e90f8c711d24
Chris Bell <cb.camellia@gmail.com>
2020-08-17 16:15:08 -0700

    Implement stringifyJSON and getElementsByClassName

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,17 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  if (obj === undefined) {
+    return '';
+  }
   if (obj === null) {
-    return "null";
+    return 'null';
   }
-  if (obj === false) {
-    return "false";
+  if (typeof(obj) === 'number' || typeof(obj) === 'boolean') {
+    return obj.toString();
   }
-  if (obj === 0) {
-    return "0";
+  if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
   }
-  // Make array into string
-  // Make object into string
-  //`${key}: ${obj[key]}` recursively
-
+  if (Array.isArray(obj)) {
+    var arrayString = '[';
+    for (var i = 0; i < obj.length; i ++) {
+      arrayString += stringifyJSON(obj[i]);
+      if (i < obj.length - 1) {
+        arrayString += ',';
+      }
+    }
+    arrayString += ']';
+    return arrayString;
+  }
+  if (typeof(obj) === 'object' && !Array.isArray(obj)) {
+    var objectString = '{';
+    var keys = Object.keys(obj);
+    for (var i = 0; i < keys.length; i++) {
+      var value = stringifyJSON(obj[keys[i]]);
+      if (value) {
+        objectString += '"' + keys[i] + '"' + ':' + value;
+        if (i < keys.length - 1) {
+          objectString += ',';
+        }
+      }
+    }
+    objectString += '}';
+    return objectString;
+  }
 };
