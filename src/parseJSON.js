a6ed41a04b13fc39ad6bab6a5ba579c747d5c471
Chris Bell <cb.camellia@gmail.com>
2020-08-17 17:02:06 -0700

    Begin parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,25 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  // Easy
+  if (json.indexOf('null') >= 0) {
+    return null;
+  }
+  if (json.indexOf('true') >= 0) {
+    return true;
+  }
+  if (json.indexOf('false') >= 0 ) {
+    return false;
+  }
+  if (typeof Number(json[0]) === 'number') {
+    return Number(json[0]);
+  }
+
+  if (json[0] === '"') {
+    var end = json.indexOf('"', 1);
+    return json.split(1, end);
+  }
+
+  // Hard
+
 };
